af6460ec9b1b5800cf4e71caddf20cbebebd483d
Yugesh Poudel <yugman45d@gmail.com>
2024-12-26 14:39:04 +0545

    main

--- index.js
+++ index.js
@@ -1,68 +1,32 @@
-const express = require('express') //Express to import
-require('dotenv').config() //dotenv ko import
+const express = require("express");
+require("dotenv").config();
 
-//!Yo mathi ko savai import files haru matra ho
-const app = express()
-// const port = 3000
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// API Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/yugesh',(req,res)=>{
-    res.send("Instagram username: yugeshpoudel45")
-})
+app.get("/yugesh", (req, res) => {
+  res.send("Instagram username: yugeshpoudel45");
+});
 
-app.get('/info',(req,res)=>{
-    res.send("<h1>K xa khabar</h1>")
-})
-
-
-//!Yesari nai API create hudo raixa
-app.get('/github', (req,res) => {
-    res.json(githubJson)
-})
-
-// app.listen(port, () => {
-//     console.log(`Mero app is listening on port ${port}`)
-// })
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`Mero app is listening on port ${process.env.PORT}`)
-// })
-
+app.get("/info", (req, res) => {
+  res.send("<h1>K xa khabar</h1>");
+});
 
 const githubJson = {
-    "login": "yugeshpoudel45",
-    "id": 104973762,
-    "node_id": "U_kgDOBkHFwg",
-    "avatar_url": "https://avatars.githubusercontent.com/u/104973762?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/yugeshpoudel45",
-    "html_url": "https://github.com/yugeshpoudel45",
-    "followers_url": "https://api.github.com/users/yugeshpoudel45/followers",
-    "following_url": "https://api.github.com/users/yugeshpoudel45/following{/other_user}",
-    "gists_url": "https://api.github.com/users/yugeshpoudel45/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/yugeshpoudel45/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/yugeshpoudel45/subscriptions",
-    "organizations_url": "https://api.github.com/users/yugeshpoudel45/orgs",
-    "repos_url": "https://api.github.com/users/yugeshpoudel45/repos",
-    "events_url": "https://api.github.com/users/yugeshpoudel45/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/yugeshpoudel45/received_events",
-    "type": "User",
-    "site_admin": false,
-    "name": "Yugesh Poudel",
-    "company": null,
-    "blog": "",
-    "location": "Kathmandu,Nepal",
-    "email": null,
-    "hireable": true,
-    "bio": "Just playing with codes.",
-    "twitter_username": null,
-    "public_repos": 13,
-    "public_gists": 0,
-    "followers": 10,
-    "following": 54,
-    "created_at": "2022-05-05T08:04:45Z",
-    "updated_at": "2024-05-03T03:09:03Z"
-}
+  login: "yugeshpoudel45",
+  id: 104973762,
+  node_id: "U_kgDOBkHFwg",
+  avatar_url: "https://avatars.githubusercontent.com/u/104973762?v=4",
+  // other fields...
+};
+
+app.get("/github", (req, res) => {
+  res.json(githubJson);
+});
+
+// Export the app for Vercel
+module.exports = app;
